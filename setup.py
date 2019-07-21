5d6a11acc4a1e86f12456d4a9309ba10e047a8a6
Nolan Conaway <nolanbconaway@gmail.com>
2019-07-21 18:17:40 -0400

    remove inline docs in setup py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="thermometer",  # Required
+    name="thermometer",
     version="0.1",
-    packages=find_packages(),  # Required
+    packages=find_packages(),
     python_requires=">=3.5",
-    install_requires=["sqlalchemy", "python-dotenv"],  # Optional
-    extras_require={"test": ["black", "pylint", "pydocstyle"]},  # Optional
+    install_requires=["sqlalchemy", "python-dotenv"],
+    extras_require={"test": ["black", "pylint", "pydocstyle"]},
 )

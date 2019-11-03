b314de86b820830cf6943ffa63403e38c48d327b
Nolan Conaway <nolanbconaway@gmail.com>
2019-11-03 15:34:49 -0500

    put black depndency in setup

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 """Set up the thermometer module."""
+import sys
 
-from setuptools import find_packages, setup
+from setuptools import setup
+
+TEST_DEPS = ["pylint", "pydocstyle", "pytest"]
+
+assert sys.version_info.major > 2, "This package is not python 2 compatible!"
+
+if sys.version_info.minor > 5:
+    TEST_DEPS.append("black")
 
 setup(
     name="thermometer",
     version="0.2",
-    packages=find_packages(),
+    packages=["thermometer"],
     python_requires=">=3.5",
     extras_require={"test": ["pylint", "pydocstyle", "pytest"]},
     entry_points={"console_scripts": ["temperature=thermometer.cli:main"]},

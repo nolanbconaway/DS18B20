0c6b70300b7773d5c5ec1754ece0b1d9dc513fc4
Nolan Conaway <nolanbconaway@gmail.com>
2019-12-20 19:38:49 -0500

    use setup.py develop in testing

--- setup.py
+++ setup.py
@@ -4,12 +4,7 @@ from pathlib import Path
 
 from setuptools import setup
 
-TEST_DEPS = ["pylint", "pydocstyle", "pytest", "pytest-cov", "codecov"]
-
 assert sys.version_info.major > 2, "This package is not python 2 compatible!"
-
-if sys.version_info.minor > 5:
-    TEST_DEPS.append("black")
 
 setup(
     name="thermometer",
@@ -17,6 +12,5 @@ setup(
     package_dir={"": "src"},
     packages=["thermometer"],
     python_requires=">=3.5",
-    extras_require={"test": TEST_DEPS},
     entry_points={"console_scripts": ["temperature=thermometer.cli:main"]},
 )
